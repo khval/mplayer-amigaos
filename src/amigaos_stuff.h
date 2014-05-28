b899aa40598867818ba35993441d1d88a6243ecc
kjetil.hvalstrand@gmail.com <kjetil.hvalstrand@gmail.com@70723ef9-80f8-f908-8c3b-f47ec2980b13>
2014-05-28 14:22:54 +0000

    cleanup

--- src/amigaos_stuff.h
+++
@@ -1,8 +0,0 @@
-#ifndef __AMIGAOS_STUFF__
-#define __AMIGAOS_STUFF__
-
-// return -1 if a pb
-int AmigaOS_Open(int argc, char *argv[]);
-void AmigaOS_Close(void);
-void AmigaOS_ParseArg(int argc, char *argv[], int *new_argc, char ***new_argv);
-#endif
