c3fecaf6f4c0d701a513543360eb560bd88ca692
kjetil.hvalstrand@gmail.com <kjetil.hvalstrand@gmail.com@70723ef9-80f8-f908-8c3b-f47ec2980b13>
2014-06-13 16:28:30 +0000

    version string fix

--- trunk/src/version.sh
+++ trunk/src/version.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 test "$1" && extra="-$1"
 
 # Extract revision number from file used by daily tarball snapshots
@@ -8,7 +9,7 @@ svn_revision=$(cat snapshot_version 2> /dev/null)
 test $svn_revision || svn_revision=$(LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
 test $svn_revision || svn_revision=$(grep revision .svn/entries 2>/dev/null | cut -d '"' -f2)
 test $svn_revision || svn_revision=$(sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null)
-test $svn_revision && svn_revision=SVN-r$svn_revision
+# test $svn_revision && svn_revision=SVN-r$svn_revision
 test $svn_revision || svn_revision=UNKNOWN
 
 # releases extract the version number from the VERSION file
@@ -18,8 +19,10 @@ test $version || version=$svn_revision
 NEW_REVISION="#define VERSION \"${version}${extra}\""
 OLD_REVISION=$(head -n 1 version.h 2> /dev/null)
 
+echo "${svn_revision}"
+
 AMIGA_VERSION=5.5
-TITLE='#define MP_TITLE "%s LiveForIt V'$AMIGA_VERSION' - "VERSION"(C) 2000-2010 MPlayer Team\n"'
+TITLE='#define MP_TITLE "%s V'$AMIGA_VERSION' - "VERSION"(C) 2000-2010 MPlayer Team\n"'
 
 AMIGA_VERSION='#define AMIGA_VERSION "LiveForIt-MPlayer '$AMIGA_VERSION' ('`date +"%d"`.$((`date +"%m"`)).`date +"%Y"`')"'
 
